facbbe7456ca1cdcf647acf3421a5adc2990cd7c
Nay Htet Aung <nayhtetaung201733@gmail.com>
2022-03-01 00:49:36 +0630

    styling the game board

--- src/components/ActiveGame.jsx
+++ src/components/ActiveGame.jsx
@@ -1,32 +1,28 @@
 import React from "react";
-
+import classes from './ActiveGame.module.css';
 const ActiveGame = () => {
   return (
-    <>
-      <article>
-        <h2>
-          You won, <span>PLAYER NAME</span>!
-        </h2>
-        <p>Click "Start New Game" above, to start a new game!</p>
-      </article>
+    <section className={classes['active-game']}>
+    <article className={classes['game-over']}>
+      <h2>You won, <span className="winner-name">PLAYER NAME</span>!</h2>
+      <p>Click "Start New Game" above, to start a new game!</p>
+    </article>
 
-      <p>
-        It's your turn <span>PLAYER NAME</span>!
-      </p>
-      <ol>
-        <li className="disabled">X</li>
-        <li></li>
-        <li className="disabled">O</li>
+    <p>It's your turn <span className={classes['active-player-name']}>PLAYER NAME</span>!</p>
+    <ol className={classes['game-board']}>
+      <li data-col="1" data-row="1"></li>
+      <li data-col="2" data-row="1"></li>
+      <li data-col="3" data-row="1"></li>
 
-        <li></li>
-        <li className="disabled">X</li>
-        <li></li>
+      <li data-col="1" data-row="2"></li>
+      <li data-col="2" data-row="2"></li>
+      <li data-col="3" data-row="2"></li>
 
-        <li className="disabled">O</li>
-        <li></li>
-        <li></li>
-      </ol>
-    </>
+      <li data-col="1" data-row="3"></li>
+      <li data-col="2" data-row="3"></li>
+      <li data-col="3" data-row="3"></li>
+    </ol>
+  </section>
   );
 };
 
