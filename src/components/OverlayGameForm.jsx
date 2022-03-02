aa055be60ef976669086f5e5124890078ad24b68
Nay Htet Aung <nayhtetaung201733@gmail.com>
2022-03-02 15:25:12 +0630

    validation user input

--- src/components/OverlayGameForm.jsx
+++ src/components/OverlayGameForm.jsx
@@ -1,44 +1,75 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import Button from "./util-components/Button";
 import style from "./OverlayGameForm.module.css";
 const Backdrop = (props) => {
-  return <div className={style.backdrop} onClick={props.onClick}/>;
+  return <div className={style.backdrop} onClick={props.onClick} />;
 };
 
-const OverlayGameForm = ({isOpen,setModal}) => {
-  const [playName , setPlayName] = useState("");
-  if(!isOpen) return null;
+const OverlayGameForm = ({ isOpen, setModal }) => {
+  const [playName, setPlayName] = useState("");
+  const [error, setError] = useState("");
+  if (!isOpen) return null;
 
   const toggleModalHandler = () => {
     setModal(!isOpen);
+    if(!error){
+      return;
+    }
+    setPlayName("");
+    setError("");
   };
-  const submitHandler=(event) =>{
+  const submitHandler = (event) => {
     event.preventDefault();
-    const enteredPlayerName = playName;
-    console.log(enteredPlayerName)
-
-  }
+    const enteredPlayerName = playName.trim();
+    if (!enteredPlayerName) {
+      setError("Please enter a valid name");
+      return;
+    }
+    console.log(enteredPlayerName);
+  };
   return (
     <>
-      {ReactDOM.createPortal(<Backdrop onClick={toggleModalHandler}/>, document.getElementById("backdrop"))}
-      <aside className={`${style.modal} ${style['config-overlay']}`}>
-        <h2>Choose your name</h2>
-        <form onSubmit={submitHandler}>
-          <div className={style['form-control']}>
-            <label htmlFor="playername">Player name</label>
-            <input type="text" name="playername" id="playername" value={playName} onChange={(event)=>setPlayName(event.target.value)}/>
-          </div>
-          <div>
-            <Button type="button" className="btn btn-alt" onClick={toggleModalHandler}>
-              Cancel
-            </Button>
-            <Button type="submit" className="btn">
-              Confirm
-            </Button>
-          </div>
-        </form>
-      </aside>
+      {ReactDOM.createPortal(
+        <Backdrop onClick={toggleModalHandler} />,
+        document.getElementById("backdrop")
+      )}
+      {ReactDOM.createPortal(
+        <aside className={`${style.modal} ${style["config-overlay"]}`}>
+          <h2>Choose your name</h2>
+          <form onSubmit={submitHandler}>
+            <div
+              className={
+                error ? `${style['form-control']} ${style.error}` : style["form-control"]
+              }
+            >
+              <label htmlFor="playername">Player name</label>
+              <input
+                type="text"
+                name="playername"
+                id="playername"
+                required
+                value={playName}
+                onChange={(event) => setPlayName(event.target.value)}
+              />
+            </div>
+            {error && <p className={style.error}>{error}</p>}
+            <div>
+              <Button
+                type="reset"
+                className="btn btn-alt"
+                onClick={toggleModalHandler}
+              >
+                Cancel
+              </Button>
+              <Button type="submit" className="btn">
+                Confirm
+              </Button>
+            </div>
+          </form>
+        </aside>,
+        document.getElementById("modalForm")
+      )}
     </>
   );
 };
